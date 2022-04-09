97281d3cce45919690ec9b1128ad0fccd2d684e6
Alien-indigena <alien.indigena12@gmail.com>
2022-04-09 18:28:38 -0300

    enter

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var count = 0;
+
+var CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() { if(
+    count>0
+)
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
